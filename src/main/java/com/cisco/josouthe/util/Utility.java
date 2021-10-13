088af3b5a5fb24eb7afcd88ee9c1cc228ff9a6c2
jbsouthe <jbsouthe@github.com>
2021-10-13 10:15:23 -0600

    updated encoding and added a better getDateString for ISO08601 format, which seems popular in our API

--- src/main/java/com/cisco/josouthe/util/Utility.java
+++ src/main/java/com/cisco/josouthe/util/Utility.java
@@ -2,13 +2,24 @@ package com.cisco.josouthe.util;
 
 import jdk.internal.net.http.common.Pair;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Utility {
     public static String encode( String original ){
-        return original.replace("|","%7C").replace(" ", "%20");
+        return original.replace("|","%7C").replace(" ", "%20").replace(":","%3A").replace(".", "%2E").replace("-", "%2D");
+    }
+
+    public static String getDateString(long dateTime) { //ISO8601 Date (Extend) https://dencode.com/en/date/iso8601
+        return ZonedDateTime.ofInstant(new Date(dateTime).toInstant(), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+    }
+    public static String getEncodedDateString( long dateTime ) {
+        return encode(getDateString(dateTime));
     }
 
     public static Pair<String,Integer> parseTypeAndSizeString( String typeAndSize ) {
