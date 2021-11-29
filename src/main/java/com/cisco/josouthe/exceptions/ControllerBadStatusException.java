53c232bd6614bc35546d00a489d00ff215c73b1b
jbsouthe <jbsouthe@github.com>
2021-11-29 10:33:27 -0700

    changed response handling on httpclient to use a ResponseHandler object, this may solve a memory leak on connections; also added a retry on request errors

--- src/main/java/com/cisco/josouthe/exceptions/ControllerBadStatusException.java
+++ src/main/java/com/cisco/josouthe/exceptions/ControllerBadStatusException.java
@@ -1,10 +1,16 @@
 package com.cisco.josouthe.exceptions;
 
-public class ControllerBadStatusException extends Exception{
+import java.io.IOException;
+
+public class ControllerBadStatusException extends IOException {
     public String urlRequestString, responseJSON;
     public ControllerBadStatusException( String message, String json, String url) {
         super(message);
         this.responseJSON = json;
         this.urlRequestString = url;
+    }
+
+    public void setURL(String uri) {
+        this.urlRequestString=uri;
     }
 }
