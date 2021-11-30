b6655790d6e794933ac2463050e1d6ee179334d5
jbsouthe <jbsouthe@github.com>
2021-11-29 17:06:14 -0700

    changed default behavior of application metric to not compress data but to instead return more records

--- src/main/java/com/cisco/josouthe/data/metric/ApplicationMetric.java
+++ src/main/java/com/cisco/josouthe/data/metric/ApplicationMetric.java
@@ -1,7 +1,7 @@
 package com.cisco.josouthe.data.metric;
 
 public class ApplicationMetric {
-    public String disableDataRollup = "false";
+    public String disableDataRollup = "true";
     public String name = null;
     public ApplicationMetric( String disableDataRollup, String name) {
         if( disableDataRollup != null ) this.disableDataRollup=disableDataRollup;
