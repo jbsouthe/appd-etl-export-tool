fb373d11f24cdf338e0d033a6685685306d88f12
jbsouthe <jbsouthe@github.com>
2021-12-15 16:38:21 -0700

    created a named thread pool so we can sort these threads running all around the place

---
+++ src/main/java/com/cisco/josouthe/scheduler/NamedThreadFactory.java
@@ -0,0 +1,42 @@
+package com.cisco.josouthe.scheduler;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    public NamedThreadFactory( String prefix ) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup()
+                : Thread.currentThread().getThreadGroup();
+        namePrefix = prefix + "-"
+                + poolNumber.getAndIncrement()
+                + "-thread-";
+    }
+
+    /**
+     * Constructs a new {@code Thread}.  Implementations may also initialize
+     * priority, name, daemon status, {@code ThreadGroup}, etc.
+     *
+     * @param r a runnable to be executed by new thread instance
+     * @return constructed thread, or {@code null} if the request to
+     * create a thread is rejected
+     */
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r,
+                namePrefix + threadNumber.getAndIncrement(),
+                0);
+        if (t.isDaemon()) {
+            t.setDaemon(false);
+        }
+        if (t.getPriority() != Thread.NORM_PRIORITY) {
+            t.setPriority(Thread.NORM_PRIORITY);
+        }
+        return t;
+    }
+}
