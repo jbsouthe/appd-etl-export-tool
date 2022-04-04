8bae0187d40d1b85d47eefa0ecf4a5f0e21f8185
jbsouthe <jbsouthe@github.com>
2022-04-04 14:57:05 -0600

    added mysql support, not yet tested

---
+++ src/main/java/com/cisco/josouthe/database/mysql/AnalyticTable.java
@@ -0,0 +1,143 @@
+package com.cisco.josouthe.database.mysql;
+
+import com.cisco.josouthe.data.analytic.Field;
+import com.cisco.josouthe.data.analytic.Result;
+import com.cisco.josouthe.database.ColumnFeatures;
+import com.cisco.josouthe.database.Database;
+import com.cisco.josouthe.util.Utility;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+public class AnalyticTable extends MySQLTable implements com.cisco.josouthe.database.AnalyticTable {
+    protected static final Logger logger = LogManager.getFormatterLogger();
+
+    public AnalyticTable(Result result, Database database ) {
+        super(result.targetTable, "Analytic Table", database);
+        columns.put("startTimestamp", new ColumnFeatures("startTimestamp", "date", -1, false));
+        columns.put("endTimestamp", new ColumnFeatures("endTimestamp", "date", -1, false));
+        for(Field field: result.fields) {
+            columns.put( field.label,
+                    new ColumnFeatures(database.convertToAcceptableColumnName(field.label, columns.values()),
+                            getColumnTypeForName(field.type),
+                            getColumnSizeForName(field.type),
+                            true)
+            );
+            logger.debug("Added column for field %s column definition: %s", field.label, columns.get(field.label));
+        }
+        this.initTable();
+    }
+
+    private int getColumnSizeForName(String type) {
+        switch(type) {
+            case "string": return database.STRING_SIZE;
+            case "integer": return database.INTEGER_SIZE;
+            case "float": return database.FLOAT_SIZE;
+            case "boolean": return database.BOOLEAN_SIZE;
+            case "date": return database.DATE_SIZE;
+            default: {
+                logger.warn("Unknown data type: %s setting table column size to %s", type, database.STRING_SIZE);
+            }
+        }
+        return database.STRING_SIZE;
+    }
+
+    private String getColumnTypeForName(String type) {
+        switch(type) {
+            case "string": return database.STRING_TYPE;
+            case "integer": return database.INTEGER_TYPE;
+            case "float": return database.FLOAT_TYPE;
+            case "boolean": return database.BOOLEAN_TYPE;
+            case "date": return database.DATE_TYPE;
+            default: {
+                logger.warn("Unknown data type: %s setting table column type to %s", type, database.STRING_TYPE);
+            }
+        }
+        return database.STRING_TYPE;
+    }
+
+    @Override
+    public int insert(Object object) {
+        Result result = (Result) object;
+        int counter=0;
+        StringBuilder sqlBeginning = new StringBuilder(String.format("insert into %s (",name));
+        StringBuilder sqlEnding = new StringBuilder(") VALUES (");
+        for( String key : columns.keySet()) {
+            sqlBeginning.append(" ").append( getColumns().get(key).name ).append(",");
+            if( columns.get(key).type.equals("date") ) {
+                sqlEnding.append("FROM_UNIXTIME(?),");
+            } else {
+                sqlEnding.append("?,");
+            }
+        }
+        StringBuilder insertSQL = new StringBuilder(sqlBeginning.substring(0, sqlBeginning.length()-1));
+        insertSQL.append( sqlEnding.substring(0, sqlEnding.length()-1) );
+        insertSQL.append(")");
+        logger.trace("Data to insert: %s", result.toString());
+        logger.trace("insertAnalytics SQL: %s", insertSQL);
+        this.initTable();
+        try ( Connection conn = database.getConnection(); PreparedStatement preparedStatement = conn.prepareStatement(insertSQL.toString());){
+            for( int dataIndex=0; dataIndex<result.results.length; dataIndex++) {
+                int parameterIndex = 1;
+                for (String key : columns.keySet()) {
+                    switch (key) {
+                        case "startTimestamp": { preparedStatement.setLong(parameterIndex++, result.startTimestamp); break; }
+                        case "endTimestamp": { preparedStatement.setLong(parameterIndex++, result.endTimestamp); break; }
+                        default: {
+                            int fieldIndex=0;
+                            for (Field field : result.fields) {
+                                if( field.label.equals(key)) {
+                                    Object data = result.results[dataIndex][fieldIndex];
+                                    if( data == null ) {
+                                        preparedStatement.setObject(parameterIndex++, data);
+                                    } else {
+                                        if( data instanceof Integer ) {
+                                            preparedStatement.setInt(parameterIndex++, (Integer) data);
+                                        } else if( data instanceof Long ) {
+                                            preparedStatement.setLong(parameterIndex++, (Long) data);
+                                        } else if( data instanceof Double ) {
+                                            preparedStatement.setDouble(parameterIndex++, (Double) data);
+                                        } else if( data instanceof  Float ) {
+                                            preparedStatement.setFloat(parameterIndex++, (Float) data);
+                                        } else if( data instanceof Boolean ) {
+                                            preparedStatement.setInt(parameterIndex++, ((Boolean)data? 1 : 0 ));
+                                        } else if( data instanceof String && Utility.isThisStringADate((String)data) ) {
+                                            //logger.trace("This String is a date: %s",data);
+                                            try {
+                                                preparedStatement.setLong(parameterIndex++, Utility.parseDateString((String) data));
+                                            } catch (SQLException exception) {
+                                                throw exception;
+                                            } catch (ParseException e) {
+                                                logger.warn("Parse exception for date time conversion: %s Exception: %s",data, e.getMessage());
+                                            }
+                                        } else {
+                                            //logger.trace("This String/Thing is not a date: %s",data);
+                                            preparedStatement.setString(
+                                                    parameterIndex++,
+                                                    fitToSize(String.valueOf(data), getColumn(key).size)
+                                                    /* this is a bit obtuse, and better as a method for other uses
+                                                    String.valueOf(data).substring(
+                                                            0,
+                                                            Math.min(getColumns().get(key).size, String.valueOf(data).length())
+                                                    )*/
+                                            );
+                                        }
+                                    }
+                                }
+                                fieldIndex++;
+                            }
+                        }
+                    }
+                }
+                counter += preparedStatement.executeUpdate();
+            }
+        } catch (Exception exception) {
+            logger.error("Error inserting analytics data into %s, Exception: %s", name, exception.toString());
+        }
+        return counter;
+    }
+}
---
+++ src/main/java/com/cisco/josouthe/database/mysql/MySQLDatabase.java
@@ -0,0 +1,228 @@
+package com.cisco.josouthe.database.mysql;
+
+import com.cisco.josouthe.Configuration;
+import com.cisco.josouthe.data.analytic.Result;
+import com.cisco.josouthe.database.ColumnFeatures;
+import com.cisco.josouthe.database.Database;
+import com.cisco.josouthe.database.Table;
+import com.cisco.josouthe.exceptions.InvalidConfigurationException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class MySQLDatabase extends Database {
+    private static final Logger logger = LogManager.getFormatterLogger();
+
+    public static final String STRING_TYPE = "varchar";
+    public static final int STRING_SIZE = 120;
+    public static final String INTEGER_TYPE = "bigint";
+    public static final int INTEGER_SIZE = 20;
+    public static final String DATE_TYPE = "datetime";
+    public static final int DATE_SIZE = -1;
+    public static final String FLOAT_TYPE = "decimal";
+    public static final int FLOAT_SIZE = 20;
+    public static final String BOOLEAN_TYPE = "tinyint";
+    public static final int BOOLEAN_SIZE = 1;
+
+    private HikariConfig hikariConfig;
+    private HikariDataSource dataSource;
+
+
+    public MySQLDatabase(Configuration configuration, String connectionString, String user, String password, String metricTable, String controlTable, String eventTable, String baselineTable, Long firstRunHistoricNumberOfHours) throws InvalidConfigurationException {
+        super( configuration, connectionString, user, password);
+        this.hikariConfig = new HikariConfig();
+        this.hikariConfig.setJdbcUrl(connectionString);
+        this.hikariConfig.setUsername(user);
+        this.hikariConfig.setPassword(password);
+        this.hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+        this.hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        this.hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        this.hikariConfig.addDataSourceProperty("maximumPoolSize", this.configuration.getProperty("scheduler-NumberOfDatabaseThreads", "10"));
+        this.hikariConfig.addDataSourceProperty("connectionTimeout", "60000");
+        this.hikariConfig.addDataSourceProperty("leakDetectionThreshold", "35000");
+        this.dataSource = new HikariDataSource(this.hikariConfig);
+        if( ! "".equals(metricTable) && isValidDatabaseTableName(metricTable) ) {
+            logger.debug("Default Metric Table set to: %s", metricTable);
+            this.defaultMetricTable = new MetricTable(metricTable, this);
+        }
+        if( ! "".equals(eventTable) && isValidDatabaseTableName(eventTable) ) {
+            logger.debug("Default Event Table set to: %s", eventTable);
+            this.defaulEventTable = new EventTable(eventTable, this);
+        }
+        if( ! "".equals(baselineTable) && isValidDatabaseTableName(baselineTable) ) {
+            logger.debug("Default Baseline Table set to: %s", baselineTable);
+            this.defaultBaselineTable = new BaselineTable(baselineTable, this);
+        }
+        if( ! "".equals(controlTable) && isValidDatabaseTableName(controlTable) ) {
+            logger.debug("Run Control Table set to: %s", controlTable);
+            this.controlTable = new ControlTable(controlTable, this);
+        }
+        if( firstRunHistoricNumberOfHours != null ) ((ControlTable)this.controlTable).setDefaultLoadNumberOfHoursIfControlRowMissing(firstRunHistoricNumberOfHours.intValue());
+        this.tablesMap = new HashMap<>();
+        logger.info("Testing Database connection returned: "+ isDatabaseAvailable());
+    }
+    public ControlTable getControlTable() { return (ControlTable) controlTable; }
+
+    public boolean isDatabaseAvailable() {
+        try (Connection conn = getConnection(); ){
+            if( conn != null ) return true;
+        } catch (Exception exception) {
+            logger.error("Error testing database connection settings, Exception: %s", exception.toString());
+        }
+        return false;
+    }
+
+    protected com.cisco.josouthe.database.MetricTable getMetricTable(String name ) {
+        if( name != null ) {
+            if( ! this.tablesMap.containsKey(name) ) {
+                Table table = new MetricTable(name, this);
+                this.tablesMap.put(name,table);
+            }
+            return (com.cisco.josouthe.database.MetricTable) this.tablesMap.get(name);
+        }
+        return (com.cisco.josouthe.database.MetricTable) this.defaultMetricTable;
+    }
+
+    protected com.cisco.josouthe.database.BaselineTable getBaselineTable(String name ) {
+        if( name != null ) {
+            if( ! this.tablesMap.containsKey(name) ) {
+                Table table = new BaselineTable(name, this);
+                this.tablesMap.put(name,table);
+            }
+            return (com.cisco.josouthe.database.BaselineTable) this.tablesMap.get(name);
+        }
+        return (com.cisco.josouthe.database.BaselineTable) this.defaultBaselineTable;
+    }
+
+    protected com.cisco.josouthe.database.EventTable getEventTable(String name ) {
+        if( name != null  ) {
+            if (!this.tablesMap.containsKey(name)) {
+                Table table = new EventTable(name, this);
+                this.tablesMap.put(name,table);
+            }
+            return (com.cisco.josouthe.database.EventTable) this.tablesMap.get(name);
+        }
+        return (com.cisco.josouthe.database.EventTable) this.defaulEventTable;
+    }
+
+    protected com.cisco.josouthe.database.AnalyticTable getAnalyticTable(Result result ) {
+        if( !this.tablesMap.containsKey(result.targetTable) ) {
+            com.cisco.josouthe.database.AnalyticTable analyticTable = new AnalyticTable( result, this);
+            this.tablesMap.put(result.targetTable, (Table) analyticTable);
+        }
+        return (com.cisco.josouthe.database.AnalyticTable) this.tablesMap.get(result.targetTable);
+    }
+    public Connection getConnection() throws SQLException {
+        logger.trace("Getting Connection to DB for user %s",this.user);
+        int tries=0;
+        boolean succeeded = false;
+        Connection connection = null;
+        while( !succeeded && tries < 30 ) {
+            try {
+                tries++;
+                connection = this.dataSource.getConnection();
+                succeeded=true;
+            } catch (SQLException sqlException) {
+                logger.warn("Error getting a connection try %d: %s", tries ,sqlException.toString());
+            }
+        }
+        if(!succeeded) {
+            throw new SQLException(String.format("Could not get a database connection! in %d tries", tries));
+        }
+        return connection;
+    }
+
+    @Override
+    public String convertToAcceptableColumnName(String label, Collection<ColumnFeatures> existingColumns) {
+        if( label == null ) return null;
+        if(_tableNameForbiddenWords == null) {
+            try {
+                loadTableNameInvalidWords();
+            } catch (InvalidConfigurationException e) {
+                logger.warn("Error Loading reserved word list for this %s database: %s", this.vendorName, e.getMessage());
+                return label;
+            }
+        }
+        label = label.replace('.', '_');
+        if(! Character.isAlphabetic(label.charAt(0))) label = "a"+label;
+        StringBuilder result = new StringBuilder();
+        label.chars().filter( ch -> ch == '_' || Character.isAlphabetic(ch) || Character.isDigit(ch)).forEach( ch -> result.append((char) ch));
+        if( _tableNameForbiddenWords.contains(result.toString().toLowerCase()) ) result.append("_c");
+        if( containsColumnAlready(result.toString(), existingColumns) ) result.append("_2");
+        if( result.length() > 30 ) {
+            String shorterLabel = result.substring(0,30);
+            if( _tableNameForbiddenWords.contains(shorterLabel.toLowerCase()) ) {
+                shorterLabel = shorterLabel.substring(0,28)+"_c"; //this could only happen if a reserved word is 30 characters long!
+            }
+            logger.trace("Setting database column name to short version: %s",shorterLabel);
+            if( containsColumnAlready(shorterLabel, existingColumns) ) shorterLabel = shorterLabel.substring(0,28) + "_2";
+            return shorterLabel;
+        }
+        logger.trace("Returning database column name: %s for field label %s", result, label);
+        return result.toString();
+    }
+
+    private boolean containsColumnAlready(String name, Collection<ColumnFeatures> existingColumns) {
+        for( ColumnFeatures columnFeatures : existingColumns)
+            if( columnFeatures.name.toLowerCase().equals(name.toLowerCase()) ) return true;
+        return false;
+    }
+
+    private List<String> _tableNameForbiddenWords = null;
+    public boolean isValidDatabaseTableName( String tableName ) throws InvalidConfigurationException {
+        if( tableName == null ) return false;
+        if(_tableNameForbiddenWords == null) loadTableNameInvalidWords();
+        if( tableName.length() > 64 ) throw new InvalidConfigurationException(String.format("Database table name longer than max 64 characters, by like %d characters. change this name: %s",(tableName.length()-64),tableName));
+        if(! Character.isAlphabetic(tableName.charAt(0))) throw new InvalidConfigurationException(String.format("Database table name must begin with an alphabetic character, not this thing '%s' change this table: %s",tableName.charAt(0),tableName));
+        //long countSpecialCharacters = tableName.chars().filter(ch -> ch == '#' || ch == '$' || ch == '_').count();
+        //long countInvalidCharacters = tableName.chars().filter(ch -> ch != '#' && ch != '$' && ch != '_' && ! Character.isAlphabetic(ch) && ! Character.isDigit(ch)).count();
+        long countInvalidCharacters = tableName.chars().filter(ch -> ch != '_' && ! Character.isAlphabetic(ch) && ! Character.isDigit(ch)).count();
+        //if( countSpecialCharacters > 1 || countInvalidCharacters > 0 ) throw new InvalidConfigurationException(String.format("Only one of the special characters ($, #, or _) is allowed and the rest must be alphanumeric only, not my rules, (shrug), change this table name: %s",tableName));
+        //if( countInvalidCharacters > 0 ) throw new InvalidConfigurationException(String.format("Only one of the special characters ('_') is allowed and the rest must be alphanumeric only, change this table name: %s",tableName));
+        if( _tableNameForbiddenWords.contains(tableName.toLowerCase()) ) throw new InvalidConfigurationException(String.format("The table name is a reserved word for this database vendor %s, please picking something different than: %s",this.vendorName, tableName));
+        return true; //if we made it this far without an exception, we are good to go
+    }
+
+    @Override
+    public String convertToAcceptableTableName(String tableName) {
+        tableName = tableName.replace('.', '_');
+        if(! Character.isAlphabetic(tableName.charAt(0))) tableName = "a"+tableName;
+        StringBuilder result = new StringBuilder();
+        tableName.chars().filter( ch -> ch == '_' || Character.isAlphabetic(ch) || Character.isDigit(ch)).forEach( ch -> result.append((char) ch));
+        try {
+            if (_tableNameForbiddenWords == null) loadTableNameInvalidWords();
+            if (_tableNameForbiddenWords.contains(result.toString().toLowerCase())) result.append("_c");
+        } catch (InvalidConfigurationException e) {
+            logger.warn("Was not able to load list of table name forbidden words: %s", e.getMessage());
+        }
+        tableName = result.toString();
+        if( tableName.length() > 30 )
+            tableName = tableName.substring(0,30);
+        return tableName;
+    }
+
+    private void loadTableNameInvalidWords() throws InvalidConfigurationException {
+        String filename = this.vendorName.toLowerCase() + "-forbidden-words.txt";
+        logger.debug("Loading file: %s",filename);
+        try (BufferedReader reader = new BufferedReader( new InputStreamReader(getClass().getClassLoader().getResourceAsStream(filename)))) {
+            _tableNameForbiddenWords = new ArrayList<>();
+            String word;
+            while ( (word = reader.readLine()) != null )
+                _tableNameForbiddenWords.add(word.toLowerCase());
+        } catch (IOException e) {
+            logger.warn("Error reading list of forbidden table names from internal file %s, this database may not be supported: %s Exception: %s", filename, this.vendorName, e.getMessage());
+            throw new InvalidConfigurationException(String.format("Error reading list of forbidden table names from internal file %s, this database may not be supported: %s", filename, this.vendorName));
+        }
+    }
+}
