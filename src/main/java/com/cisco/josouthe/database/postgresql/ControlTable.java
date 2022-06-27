b337ede16085d431a8269a7ac9f28fd3c7aa9d67
jbsouthe <jbsouthe@github.com>
2022-06-27 16:46:05 -0600

    added postgresql support, yet to test

---
+++ src/main/java/com/cisco/josouthe/database/postgresql/ControlTable.java
@@ -0,0 +1,79 @@
+package com.cisco.josouthe.database.postgresql;
+
+import com.cisco.josouthe.database.ColumnFeatures;
+import com.cisco.josouthe.database.ControlEntry;
+import com.cisco.josouthe.database.Database;
+import com.cisco.josouthe.util.Utility;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ControlTable extends PGSQLTable implements com.cisco.josouthe.database.ControlTable {
+    protected static final Logger logger = LogManager.getFormatterLogger();
+    private int defaultLoadNumberOfHoursIfControlRowMissing = 48;
+
+    public ControlTable( String tableName, Database database ) {
+        super(tableName,"Control Table", database);
+        columns.put("controller", new ColumnFeatures("controller", "varchar", 120, false, true));
+        columns.put("application", new ColumnFeatures("application", "varchar", 120, false, true));
+        columns.put("datatype", new ColumnFeatures("datatype", "varchar", 50, false, true));
+        columns.put("lastruntimestamp", new ColumnFeatures("lastruntimestamp", "bigint", 20, false));
+        initTable();
+    }
+
+    @Override
+    public void setDefaultLoadNumberOfHoursIfControlRowMissing(int hours) { this.defaultLoadNumberOfHoursIfControlRowMissing=hours; }
+
+    @Override
+    public int insert(Object object) {
+        return 0;
+    }
+
+    @Override
+    public ControlEntry getLastRunTimestamp(String controller, String application, String dataType) {
+        ControlEntry controlEntry = new ControlEntry();
+        controlEntry.controller=controller;
+        controlEntry.application=application;
+        controlEntry.type=dataType;
+        Long timeStamp = null;
+        try (Connection conn = database.getConnection(); Statement statement = conn.createStatement(); ){
+            String query = String.format(" select lastRunTimestamp from %s where lower(controller) like lower(\"%s\") and lower(application) like lower(\"%s\") and lower(dataType) like lower(\"%s\") ", this.name, controller, application, dataType);
+            ResultSet resultSet = statement.executeQuery(query);
+            if( resultSet.next() ) {
+                timeStamp = resultSet.getLong(1);
+            }
+            resultSet.close();
+        } catch (SQLException exception) {
+            logger.error("Error getting last run time from table %s for %s:%s(%s), Exception: %s", getName(), controller, application, dataType, exception.toString());
+        }
+        if( timeStamp != null ) {
+            controlEntry.timestamp = timeStamp;
+        } else {
+            controlEntry.timestamp = Utility.now() - (this.defaultLoadNumberOfHoursIfControlRowMissing * 60 * 60 * 1000);
+        }
+        return controlEntry;
+    }
+
+    @Override
+    public synchronized int setLastRunTimestamp(ControlEntry controlEntry) {
+        try (Connection conn = database.getConnection(); Statement statement = conn.createStatement();){
+            /*
+            StringBuilder update = new StringBuilder(String.format(" merge %s t using dual on ( lower(t.controller) = '%s' and lower(t.application) = '%s' and lower(t.dataType) = '%s' )", this.name, controlEntry.controller.toLowerCase(), controlEntry.application.toLowerCase(), controlEntry.type.toLowerCase() ));
+            update.append(String.format(" when not matched by target then insert (controller,application,dataType,lastRunTimestamp) values ('%s','%s','%s',%d) ", controlEntry.controller, controlEntry.application, controlEntry.type, controlEntry.timestamp));
+            update.append(String.format(" when matched then update set lastRunTimestamp = %d",controlEntry.timestamp));
+             */
+            StringBuilder update = new StringBuilder(String.format("insert into %s (controller,application,dataType,lastRunTimestamp) values ('%s','%s','%s',%d)  ", this.name,controlEntry.controller, controlEntry.application, controlEntry.type, controlEntry.timestamp ));
+            update.append(String.format("on conflict (controller,application,dataType) do update set lastRunTimestamp = %d", controlEntry.timestamp));
+            return statement.executeUpdate(update.toString());
+        } catch (SQLException exception) {
+            logger.error("Error setting last run time into table %s for %s:%s(%s), Exception: %s", getName(), controlEntry.controller, controlEntry.application, controlEntry.type, exception.toString());
+        }
+        return 0;
+    }
+
+
+}
---
+++ src/main/java/com/cisco/josouthe/database/postgresql/EventTable.java
@@ -0,0 +1,74 @@
+package com.cisco.josouthe.database.postgresql;
+
+import com.cisco.josouthe.data.event.EventData;
+import com.cisco.josouthe.database.ColumnFeatures;
+import com.cisco.josouthe.database.Database;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+
+public class EventTable extends PGSQLTable implements com.cisco.josouthe.database.EventTable {
+    protected static final Logger logger = LogManager.getFormatterLogger();
+
+    public EventTable( String tableName, Database database ) {
+        super(tableName, "Event Table", database);
+        columns.put("controller", new ColumnFeatures("controller", "varchar", 50, false));
+        columns.put("application", new ColumnFeatures("application", "varchar", 50, false));
+        columns.put("id", new ColumnFeatures("id", "bigint", 20, false));
+        columns.put("eventtime", new ColumnFeatures("eventtime", "bigint", 20, false));
+        columns.put("type", new ColumnFeatures("type", "varchar", 50, false));
+        columns.put("subtype", new ColumnFeatures("subtype", "varchar", 50, true));
+        columns.put("severity", new ColumnFeatures("severity", "varchar", 20, false));
+        columns.put("summary", new ColumnFeatures("summary", "varchar", 120, true));
+        columns.put("triggeredentityid", new ColumnFeatures("triggeredentityid", "bigint", 20, false));
+        columns.put("triggeredentityname", new ColumnFeatures("triggeredentityname", "varchar", 120, true));
+        columns.put("triggeredentitytype", new ColumnFeatures("triggeredentitytype", "varchar", 120, false));
+        columns.put("eventtimestamp", new ColumnFeatures("eventtimestamp", "timestamp", -1, false));
+        this.initTable();
+    }
+
+    @Override
+    public int insert(Object object) {
+        EventData event = (EventData) object;
+        int counter=0;
+        StringBuilder insertSQL = new StringBuilder(String.format("insert into %s (",name));
+        insertSQL.append("controller, application, id, eventTime, type, subtype, severity, summary, triggeredEntityId, triggeredentityname, triggeredentitytype, eventtimestamp");
+        insertSQL.append(") VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+        logger.trace("insertMetric SQL: %s",insertSQL);
+        try ( Connection conn = database.getConnection(); PreparedStatement preparedStatement = conn.prepareStatement(insertSQL.toString());){
+            int parameterIndex = 1;
+            preparedStatement.setString(parameterIndex++, fitToSize(event.controllerHostname, "controller"));
+            preparedStatement.setString(parameterIndex++, fitToSize(event.applicationName, "application"));
+            preparedStatement.setLong(parameterIndex++, event.id);
+            preparedStatement.setLong(parameterIndex++, event.eventTime);
+            preparedStatement.setString(parameterIndex++, fitToSize(event.type, "type"));
+            preparedStatement.setString(parameterIndex++, fitToSize(event.subType, "subtype"));
+            preparedStatement.setString(parameterIndex++, fitToSize(event.severity, "severity"));
+            preparedStatement.setString(parameterIndex++, fitToSize(event.summary, "summary"));
+            if( event.triggeredEntity != null ) {
+                preparedStatement.setInt(parameterIndex++, event.triggeredEntity.entityId);
+                preparedStatement.setString(parameterIndex++, fitToSize(event.triggeredEntity.name,"triggeredentityname"));
+                preparedStatement.setString(parameterIndex++, fitToSize(event.triggeredEntity.entityType, "triggeredentitytype"));
+            } else if( event.affectedEntities != null ) {
+                    preparedStatement.setInt(parameterIndex++, event.affectedEntities.get(0).entityId);
+                    preparedStatement.setString(parameterIndex++, fitToSize(event.affectedEntities.get(0).name, "triggeredentityname"));
+                    preparedStatement.setString(parameterIndex++, fitToSize(event.affectedEntities.get(0).entityType, "triggeredentitytype"));
+            } else {
+                preparedStatement.setInt(parameterIndex++, -1);
+                preparedStatement.setString(parameterIndex++, "");
+                preparedStatement.setString(parameterIndex++, "");
+            }
+            preparedStatement.setTimestamp(parameterIndex++, new Timestamp(event.eventTime));
+            counter += preparedStatement.executeUpdate();
+        } catch (Exception exception) {
+            logger.error("Error inserting events into %s, Exception: %s", name, exception.toString());
+            logger.warn("Bad SQL: %s",insertSQL.toString());
+        }
+        return counter;
+    }
+
+
+}
