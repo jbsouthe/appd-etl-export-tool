e3bdd61a04a33b0a581d31799941fdbf4334c552
jbsouthe <jbsouthe@github.com>
2022-06-28 11:38:47 -0600

    optimized boolean storage for oracle, from size 22 to 1

--- src/main/java/com/cisco/josouthe/database/oracle/BaselineTable.java
+++ src/main/java/com/cisco/josouthe/database/oracle/BaselineTable.java
@@ -23,7 +23,7 @@ public class BaselineTable extends OracleTable implements IBaselineTable {
         columns.put("metricname", new ColumnFeatures("metricName", "varchar2", 200, false));
         columns.put("baseline", new ColumnFeatures("baseline", "varchar2", 200, false));
         columns.put("frequency", new ColumnFeatures("frequency", "varchar2", 50, false));
-        columns.put("userange", new ColumnFeatures("userange", "number", 22, false));
+        columns.put("userange", new ColumnFeatures("userange", "number", 1, false));
         for( String columnName : new String[] { "metricid","startTimeInMillis", "occurrences", "currentValue", "min", "max", "count", "sum", "value", "standardDeviation"})
             columns.put(columnName.toLowerCase(), new ColumnFeatures(columnName, "number", 22, false));
         columns.put("startTimestamp", new ColumnFeatures("startTimestamp", "date", -1, false));
